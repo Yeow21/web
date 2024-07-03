df9695d2099be9f51b11cca6c9cff821b782784d
Yeow21 <116248968+Yeow21@users.noreply.github.com>
2024-07-03 20:13:39 +0000

    asd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-/* CARET FUNCTIONALITY */
+/* CARET FUNCTIONALITY
 var toggler = document.getElementsByClassName("caret");
 var i;
 
@@ -10,3 +10,5 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("caret-down");
   });
 }
+
+*/
